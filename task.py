c4d45bdad7400964e6e429ddb80e8dd4728638d6
Rashadnuri <rashadnuri1601@gmail.com>
2024-07-29 18:36:16 +0400

    first commit

---
+++ task.py
@@ -0,0 +1,23 @@
+## 1 task
+a = True
+b = False
+## c = a * b yoxlamaq və nəticə haqqında düşünmək 
+## c = a + b yoxlamaq və nəticə haqqında düşünmək     
+## c = d - b yoxlamaq və nəticə haqqında düşünmək
+## c = d / b yoxlamaq və nəticə haqqında düşünmək
+
+## 2 task 
+a = 3
+b = 5 
+c= b**3
+print (c)
+## b - dən istifadə etmədən a-nın nəticəsini 8 almaq 
+## c - dən istifadə edərək a-nı kvadratına yüksəlmək 
+## c - dən istifadə edərək b-ni üstü 3 qiymətə yüksəlmək 
+
+## 3 task 
+a = 1
+b = 0 
+## a və b arasında and, or, not əməliyyaltları aparmaq və nəticə haqqında düşünmək 
+
+## hər bir taskı ayrı ayrı fayllarda edib mənə göndəriniz Suna xanım :)
---
+++ test.py
@@ -0,0 +1,9 @@
+list1 = ["alma" , 3 , True]
+print (list1)
+a = [2]
+b=[5,6]
+print(a)
+
+
+x = [ 1 , 2 , 3]
+print (x[-1]) 
