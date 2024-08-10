06137b166e8bdd56c01feef8e91833f91ecb8991
Rashadnuri <rashadnuri1601@gmail.com>
2024-08-10 16:22:21 +0400

    task-update

--- task.py
+++ task.py
@@ -1,8 +1,6 @@
-a = [1, 2, 3, 4, 5, 6, 7, 8 ,9 ,10 ,11 ,12 ,13 ,14]
-b = [14, 24, 34, 44, 54, 64, 74, 84 ,94 ,104 ,114 ,124 ,134 ,144]
-c = ["alma" , "hello" , True , 55 , "python" , 44 , False]
- 
-## c listinin ilk elementini salam sözü ilə dəyişmək 
-## с listinin son elementini 11 rəqəmi ilə dəyişmək 
-## a listinin son 4 elementini hərflərlə dəyişmək 
-## b listinin ilk 3 elementini sözlərlə əvəz etmək 
+## boş bir list qurub, append metodundan istifadə edərək listə həm string həm int həm də bool elementi daxil etmək
+## 1-ci taskı etdikdən sonra əlavə etdiyiniz elementlərin üzərinə listin 2-ci elementi olacaq şəkildə yeni element daxil etmək
+## 2-ci taskı etdikdən sonra listin ilk elementini onun adından istifadə edərək silmək 
+## eyni listin son elementini silmək 
+## eyni listin hər hansısa elementini indeksini göstərərək silmək (hər iki üsul ilə) 
+## sonda listin daxilindəki bütün elementləri silmək 
--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-a = ["alma" , 3 , True]
-a[0] = "armud"
-print(a)
+list1 = []
+list1.append("salam")
+list1.append(4)
+list1.append(True)
+print(list1)
+list1.insert(1, "hi")
+print(list1)
