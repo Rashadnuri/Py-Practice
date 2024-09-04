49e268c8803d19dfd447928cc32e529c96af6dd1
Rashadnuri <rashadnuri1601@gmail.com>
2024-09-05 01:18:20 +0400

    task-update

--- task.py
+++ task.py
@@ -1,7 +1,7 @@
-## thistuple = ("salam" , 42 ,True , "hi") tupllenin sonuna 21 reqemini elave etmek 
-## thistuple = ("salam" , 42 ,True , "hi") tuple-in ortasına alma sozunu elave etmek 
-## thistuple = ("salam" , 42 ,True , "hi") birinci elementi sagol ile deyismek 
-## thistuple = ("salam" , 42 ,True , "hi") True-ni silmek 
-## thistuple = ("salam" , 42 ,True , "hi" , False, 91 , 33 , "alma") tuple-ni ele 3 hisseye bolmek ki ilk hissede daha cox element olsun 
-## thistuple = ("salam" , 42 ,True , "hi") tuple-nin elementlerini ayri ayri cap etmek 
-## thistuple = ("salam" , 42 ,True , "hi") tuplenin elementlerini 4 defe tekrar eletdirmek (tuple icinde)
+## thisset = {1, "salam" , 52 , True, "0"} setinin uzunluqunu teyin etmek 
+## thisset = {1, "salam" , 52 , True, "0"} setine 0 ve false deyerleri daxil edib uzunlugunu cap etmek 
+## her-hansi iki seti birlesdirmek 
+## iki oxsar elementli setlerden ferqli elementleri cap etmek 
+## setden element silmek 
+## setin daxilindeki butun elementleri silmek 
+## setdeki butun elementleri cap etmek 
---
+++ test.py
@@ -0,0 +1,9 @@
+set1 = {"apple", "banana" , "cherry"}
+set2 = {"google", "microsoft", "apple"}
+  
+  
+thislist = [2,5,3]
+thisset = set(thislist)
+print(thisset)
+set3 = set2 - set1
+print(set3)
